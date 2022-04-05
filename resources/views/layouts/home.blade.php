ec528e88773c522ce050d46aa09a58002f19380c
kemre8848 <kemre8848@gmail.com>
2022-04-05 12:02:09 +0300

    template implement

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Site Metas -->
+<title>SmartEDU - Education Responsive HTML5 Template</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Site Icons -->
+<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<!-- Site CSS -->
+<link rel="stylesheet" href="assets/style.css">
+<!-- ALL VERSION CSS -->
+<link rel="stylesheet" href="assets/css/versions.css">
+<!-- Responsive CSS -->
+<link rel="stylesheet" href="assets/css/responsive.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="assets/css/custom.css">
+
+<!-- Modernizer for Portfolio -->
+<script src="assets/js/modernizer.js"></script>
+
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body class="host_version">
+@include('home.header')
+@section('slider')
+@show
+@section('content')
+    @show
+
+
+@include('home.footer')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,12 +11,27 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-| Test Comment
+| TescdsaLVCFDSKVCNS
 */
-
-Route::get('/', function () {
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+    Route::get('/welcome', function () {
     return view('welcome');
 });
+//- Call Controller Function
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+//- Route-> Controller Function
+Route::get('/test',[HomeController::class,'test'])->name('test');
+
+//- Route with parameters
+Route::get('/param/{id}/{number}',[HomeController::class,'param'])->name('param');
+
+
+// Routh with post
+Route::post('/save',[HomeController::class,'save'])->name('save');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
