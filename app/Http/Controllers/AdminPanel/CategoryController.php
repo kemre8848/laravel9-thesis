0da906c06be47a01cd40d6183c7978b89edfd664
kemre8848 <kemre8848@gmail.com>
2022-04-18 22:57:52 +0300

    10 Laravel Category Insert Data to Database

---
+++ app/Http/Controllers/AdminPanel/CategoryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\AdminPanel;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Photo;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('admin.category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = new Category();
+        $data-> title = $request->title;
+        $data-> keywords = $request->keywords;
+        $data-> description = $request->description;
+        $data-> status = $request->status;
+        $data-> send();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Photo $photo)
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Photo $photo)
+    {
+        //
+    }
+}
